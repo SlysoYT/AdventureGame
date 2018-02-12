6c7d932cf49de3ecca60af7e0c333180c7d8d99c
Thomas <Thomas@PC-Thomas.home>
2018-02-12 14:48:38 +0100

    If there is no player moves randomly around

--- src/game/entity/mob/Slime.java
+++ src/game/entity/mob/Slime.java
@@ -1,5 +1,6 @@
 package game.entity.mob;
 
+import game.Game;
 import game.entity.mob.player.Player;
 import game.entity.spawner.ParticleSpawner;
 import game.graphics.Screen;
@@ -9,6 +10,9 @@ import game.util.Hitbox;
 
 public class Slime extends Mob
 {
+	private float xChange = 0, yChange = 0;
+	private int xGoal, yGoal;
+
 	private float jumpHeight = 3.5F;
 	private float jumpHeightOffset = 0.0F;
 	private float yVelocity = jumpHeight;
@@ -16,36 +20,54 @@ public class Slime extends Mob
 
 	private boolean shouldIncreaseY = false;
 	private boolean shouldDecreaseY = false;
-	private byte delay = 0;
+	private byte jumpDelay = 0;
 
 	private int xOffsetRandom = rand.nextInt(40) - 20;
 
 	public Slime(int x, int y, float speed)
 	{
 		initMob(x, y, new Hitbox(-7, -1, 13, 7), Sprite.slimeDown, 20.0F, speed, 10.0F, 30);
+		xGoal = x;
+		yGoal = y;
 	}
 
 	public void tick()
 	{
 		tickMob();
 
-		//Movement
-		if(delay > 0)
+		xChange = 0;
+		yChange = 0;
+
+		if(level.getClientPlayer() != null)
 		{
-			delay--;
-			return;
+			Player player = level.getClientPlayer();
+			yGoal = player.getY();
+
+			int distanceToPlayer = (int) Math.sqrt(Math.pow(player.getX() - this.getX(), 2) + Math.pow(player.getY() - this.getY(), 2));
+			if(distanceToPlayer > 50) xGoal = player.getX() + xOffsetRandom;
+			else xGoal = player.getX();
+		}
+		else
+		{
+			if(Math.sqrt(Math.pow(xGoal - this.getX(), 2) + Math.pow(yGoal - this.getY(), 2)) < 10)
+			{
+				int xPos = 0, yPos = 0;
+				while(true)
+				{
+					xPos = rand.nextInt(Tile.DEFAULT_TILE_SIZE * Game.getLevel().getLevelWidth());
+					yPos = rand.nextInt(Tile.DEFAULT_TILE_SIZE * Game.getLevel().getLevelHeight());
+					if(!Game.getLevel().hitboxCollidesWithSolid(xPos, yPos, new Slime(xPos, yPos, 0.5F).getHitbox())) break;
+				}
+				xGoal = xPos;
+				yGoal = yPos;
+			}
 		}
 
-		Player player = level.getClientPlayer();
-		int distanceToPlayer = (int) Math.sqrt(Math.pow(player.getX() - this.getX(), 2) + Math.pow(player.getY() - this.getY(), 2));
-
-		float xChange = 0, yChange = 0;
-
-		int xGoal;
-		int yGoal = player.getY();
-
-		if(distanceToPlayer > 50) xGoal = player.getX() + xOffsetRandom;
-		else xGoal = player.getX();
+		if(jumpDelay > 0)
+		{
+			jumpDelay--;
+			return;
+		}
 
 		if(yGoal > y) shouldDecreaseY = true;
 		else shouldDecreaseY = false;
@@ -61,13 +83,13 @@ public class Slime extends Mob
 		else
 		{
 			resetJump();
-			delay = (byte) (5 + rand.nextInt(40));
-			level.add(new ParticleSpawner((int) (x), (int) (y + 11), 0.01F, 0.01F, 30, 3, level, Sprite.PARTICLE_SLIME));
-			level.add(new ParticleSpawner((int) (x + 4), (int) (y + 11), 0.5F, 0.15F, 30, 3, level, Sprite.PARTICLE_SLIME));
-			level.add(new ParticleSpawner((int) (x - 4), (int) (y + 11), 0.5F, 0.15F, 30, 3, level, Sprite.PARTICLE_SLIME));
+			jumpDelay = (byte) (5 + rand.nextInt(40));
+			level.add(new ParticleSpawner((int) (x), (int) (y + 15), 0.01F, 0.01F, 20, 3, level, Sprite.PARTICLE_SLIME));
+			level.add(new ParticleSpawner((int) (x + 4), (int) (y + 15), 0.5F, 0.15F, 20, 3, level, Sprite.PARTICLE_SLIME));
+			level.add(new ParticleSpawner((int) (x - 4), (int) (y + 15), 0.5F, 0.15F, 20, 3, level, Sprite.PARTICLE_SLIME));
 		}
-		if(xGoal < x) xChange -= getSpeed();
-		else if(xGoal > x) xChange += getSpeed();
+		if(xGoal <= x) xChange -= getSpeed();
+		else xChange += getSpeed();
 		this.motion(xChange, yChange);
 
 		//Attacking

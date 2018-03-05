9a450475f28fa736cdb00fa6d840127cd84242ac
Thomas <Thomas@PC-Thomas.home>
2018-03-05 16:19:42 +0100

    Added more tiles

--- src/game/level/tile/WaterTile.java
+++ src/game/level/tile/WaterTile.java
@@ -14,6 +14,14 @@ public class WaterTile extends Tile
 
 	public void render(int x, int y, Screen screen)
 	{
+		boolean xEven = x % 2 == 0;
+		boolean yEven = y % 2 == 0;
+
+		if(xEven && yEven) sprite = Sprite.SPRITE_WATER_0;
+		else if(!xEven && yEven) sprite = Sprite.SPRITE_WATER_1;
+		else if(xEven && !yEven) sprite = Sprite.SPRITE_WATER_2;
+		else sprite = Sprite.SPRITE_WATER_3;
+
 		screen.renderTile(x << TILE_SIZE_SHIFTING, y << TILE_SIZE_SHIFTING, this);
 	}
 

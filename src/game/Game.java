91cc37242d70fda38073721ddef56b0ae4eff104
SlysoYT <thomas.zahner2000@gamil.com>
2018-04-11 17:10:42 +0200

    Added function to get the OS type

--- src/game/Game.java
+++ src/game/Game.java
@@ -454,4 +454,20 @@ public class Game extends Canvas implements Runnable
 	{
 		return gameStateTicksPassed;
 	}
+
+	public static boolean isWindows()
+	{
+		return System.getProperty("os.name").toLowerCase().contains("win");
+	}
+
+	public static boolean isUnix()
+	{
+		return System.getProperty("os.name").toLowerCase().contains("nix") || System.getProperty("os.name").toLowerCase().contains("nux")
+				|| System.getProperty("os.name").toLowerCase().contains("aix");
+	}
+
+	public static boolean isMac()
+	{
+		return System.getProperty("os.name").toLowerCase().contains("mac");
+	}
 }

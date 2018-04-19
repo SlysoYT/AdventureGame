febfbca7f85c4a12583d3561e8a9fb76c5f2aecb
SlysoYT <thomas.zahner2000@gamil.com>
2018-04-19 19:16:00 +0200

    Improved slime AI by using the path finding algorithm

--- src/game/entity/mob/Slime.java
+++ src/game/entity/mob/Slime.java
@@ -1,5 +1,6 @@
 package game.entity.mob;
 
+import java.util.List;
 import java.util.UUID;
 
 import game.Game;
@@ -9,6 +10,8 @@ import game.graphics.Screen;
 import game.graphics.Sprite;
 import game.level.tile.Tile;
 import game.util.Hitbox;
+import game.util.Node;
+import game.util.Vector2i;
 
 public class Slime extends Mob
 {
@@ -18,13 +21,15 @@ public class Slime extends Mob
 	private float jumpHeight = 3.5F;
 	private float jumpHeightOffset = 0.0F;
 	private float yVelocity = jumpHeight;
-	private float gravity = 0.25F;
+	private float gravity = 0.17F + rand.nextFloat() / 6;
 
 	private boolean shouldIncreaseY = false;
 	private boolean shouldDecreaseY = false;
 	private byte jumpDelay = 0;
 
 	private int xOffsetRandom = rand.nextInt(40) - 20;
+
+	private List<Node> path = null;
 
 	public Slime(int x, int y, UUID uuid)
 	{
@@ -38,14 +43,32 @@ public class Slime extends Mob
 		xChange = 0;
 		yChange = 0;
 
-		if(level.getClientPlayer() != null)
-		{
-			Player player = level.getClientPlayer();
-			yGoal = player.getY();
+		Player target = level.getNearestPlayer(this);
 
-			int distanceToPlayer = (int) Math.sqrt(Math.pow(player.getX() - this.getX(), 2) + Math.pow(player.getY() - this.getY(), 2));
-			if(distanceToPlayer > 50) xGoal = player.getX() + xOffsetRandom;
-			else xGoal = player.getX();
+		if(target != null)
+		{
+			yGoal = target.getY();
+
+			int distanceToPlayer = (int) Math.sqrt(Math.pow(target.getX() - this.getX(), 2) + Math.pow(target.getY() - this.getY(), 2));
+
+			/*
+			 * if(distanceToPlayer > 50) xGoal = target.getX() + xOffsetRandom;
+			 * else xGoal = target.getX();
+			 */
+
+			if(Game.getGameStateTicksPassed() % 20 == rand.nextInt(20)) //Randomness to not make all slimes tick at the same time
+			{
+				Vector2i start = new Vector2i(this.getX() >> Screen.TILE_SIZE_SHIFTING, this.getY() >> Screen.TILE_SIZE_SHIFTING);
+				Vector2i end = new Vector2i(target.getX() >> Screen.TILE_SIZE_SHIFTING, target.getY() >> Screen.TILE_SIZE_SHIFTING);
+				path = level.findPath(start, end);
+				if(path == null) return;
+				if(path.size() > 0)
+				{
+					Vector2i pathVector = path.get(path.size() - 1).tile;
+					xGoal = (pathVector.getX() << Screen.TILE_SIZE_SHIFTING) + this.getHitbox().getXOffset() + this.getHitbox().getWidth() + 1;
+					yGoal = (pathVector.getY() << Screen.TILE_SIZE_SHIFTING) + this.getHitbox().getYOffset() + this.getHitbox().getHeight() + 1;
+				}
+			}
 		}
 		else
 		{
@@ -93,10 +116,10 @@ public class Slime extends Mob
 		this.motion(xChange, yChange);
 
 		//Attacking
-		Player collidedPlayer = level.anyPlayerCollidedWithHitbox(x, y, hitbox);
-		if(collidedPlayer != null)
+		List<Player> collidedPlayers = level.playersCollidedWithHitbox(x, y, hitbox);
+		if(!collidedPlayers.isEmpty())
 		{
-			this.attack(collidedPlayer);
+			this.attack(collidedPlayers.get(0));
 		}
 	}
 

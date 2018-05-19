0841be938279024d6ce6b8e58724444803feb263
SlysoYT <thomas.zahner2000@gamil.com>
2018-05-19 16:52:30 +0200

    Multiple mobs now can be spawned via !summon command

--- src/game/chat/commands/CommandSummon.java
+++ src/game/chat/commands/CommandSummon.java
@@ -14,21 +14,32 @@ public class CommandSummon extends Command
 {
 	public CommandSummon()
 	{
-		super("summon", "summon <mob>", "Summon the specified target", (byte) 1);
+		super("summon", "summon <mob> or summon <mob> <amount>", "Summon one or multiple mobs", (byte) 1, (byte) 2);
 	}
 
 	@Override
 	protected void onEnable(List<String> args, Player sender)
 	{
-		String mob = args.get(0);
-		boolean success = true;
+		if(args.size() == 1)
+		{
+			String mob = args.get(0);
+			boolean success = true;
 
-		if(mob.equals("slime")) Game.getLevel().add(new Slime(sender.getX(), sender.getY(), null));
-		else if(mob.equals("salesman")) Game.getLevel().add(new Salesman(sender.getX(), sender.getY(), null));
-		else if(mob.equals("guardian")) Game.getLevel().add(new Guardian(sender.getX(), sender.getY(), null));
-		else success = false;
+			if(mob.equals("slime")) Game.getLevel().add(new Slime(sender.getX(), sender.getY(), null));
+			else if(mob.equals("salesman")) Game.getLevel().add(new Salesman(sender.getX(), sender.getY(), null));
+			else if(mob.equals("guardian")) Game.getLevel().add(new Guardian(sender.getX(), sender.getY(), null));
+			else success = false;
 
-		if(success) Chat.addMessage(new Message("Summoned the mob successfully!", "Server"));
-		else Chat.addMessage(new Message("Unknown mob!", "Server"));
+			if(success) Chat.addMessage(new Message("Summoned the mob successfully!", "Server"));
+			else Chat.addMessage(new Message("Unknown mob!", "Server"));
+		}
+		else
+		{
+			int amount = Integer.parseInt(args.get(1));
+			args.remove(1);
+
+			for(int i = 0; i < amount; i++)
+				onEnable(args, sender);
+		}
 	}
 }
